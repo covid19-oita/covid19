2b82de6978231f01b493b849bac0880c2fc20c47
Daiki <nojiri1098@gmail.com>
2020-03-22 19:50:33 +0900

    Replace 東京都 to 有志

--- layouts/default.vue
+++ layouts/default.vue
@@ -100,7 +100,7 @@ export default Vue.extend({
           hid: 'description',
           name: 'description',
           content: this.$tc(
-            '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、東京都が開設したものです。'
+            '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、有志が開設したものです。'
           )
         },
         {
@@ -137,7 +137,7 @@ export default Vue.extend({
           hid: 'og:description',
           property: 'og:description',
           content: this.$tc(
-            '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、東京都が開設したものです。'
+            '当サイトは新型コロナウイルス感染症 (COVID-19) に関する最新情報を提供するために、有志が開設したものです。'
           )
         },
         {
