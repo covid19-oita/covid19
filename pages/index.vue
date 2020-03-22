e620596a548e0ce9eae947a77eb6f772814b87a2
Daiki <nojiri1098@gmail.com>
2020-03-22 19:52:44 +0900

    Remove 相談の手順
    v2.0.0では表示しない

--- pages/index.vue
+++ pages/index.vue
@@ -5,12 +5,6 @@
       :title="headerItem.title"
       :date="headerItem.date"
     />
-    <static-info
-      class="mb-4"
-      :url="localePath('/flow')"
-      :text="$t('自分や家族の症状に不安や心配があればまずは電話相談をどうぞ')"
-      :btn-text="$t('相談の手順を見る')"
-    />
     <v-row class="DataBlock">
       <confirmed-cases-number-card />
       <confirmed-cases-attributes-card />
