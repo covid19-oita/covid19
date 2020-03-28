a15e1633890b5babf2c37863dca249aacc8d64f6
Shoma Okamoto <shoma.develop@gmail.com>
2020-03-28 17:46:37 +0900

    Impl. AgeCard.

---
+++ components/cards/AgeCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-col cols="12" md="6" class="DataCard">
+    <time-bar-chart
+      title="年代別陽性患者数"
+      :title-id="'patients-by-age'"
+      :chart-id="'time-bar-chart-patients-by-age'"
+      :chart-data="ageGraph"
+      :date="age.last_update"
+      :unit="'人'"
+      :url="'https://web.pref.hyogo.lg.jp/kk03/corona_hasseijyokyo.html'"
+      :show-button="false"
+    />
+  </v-col>
+</template>
+<script lang="ts">
+import age from '@/data/age.json'
+import TimeBarChart from '@/components/TimeBarChart.vue'
+import formatVariableGraph from '@/utils/formatVariableGraph'
+
+export default {
+  components: {
+    TimeBarChart
+  },
+  data() {
+    const ageGraph = formatVariableGraph(age.data)
+    const data = {
+      ageGraph,
+      age
+    }
+    return data
+  }
+}
+</script>
