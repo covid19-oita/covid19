acc20a52eff7ae4f7c97c4f43daf5497ca126072
Daiki <nojiri1098@gmail.com>
2020-04-16 14:48:16 +0900

    Remove NewsListItem

--- components/cards/NewsCard.vue
+++ components/cards/NewsCard.vue
@@ -8,34 +8,59 @@
         {{ $t('最新のお知らせ') }}
       </h3>
     </div>
-    <news-list-item :items="newsItems" />
+    <ul class="WhatsNew-list">
+      <li v-for="(item, i) in items" :key="i" class="WhatsNew-list-item">
+        <a
+          class="WhatsNew-list-item-anchor"
+          :href="item.url"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <time
+            class="WhatsNew-list-item-anchor-time px-2"
+            :datetime="item.date"
+          >
+            {{ item.date }}
+          </time>
+          <span class="WhatsNew-list-item-anchor-link">
+            {{ item.text }}
+            <v-icon
+              v-if="!isInternalLink(item.url)"
+              class="WhatsNew-item-ExternalLinkIcon"
+              size="12"
+            >
+              mdi-open-in-new
+            </v-icon>
+          </span>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
-import NewsListItem from '@/components/NewsListItem.vue'
+import Vue from 'vue'
 
-export default {
-  components: {
-    NewsListItem
-  },
+export default Vue.extend({
   data() {
     return {
-      newsItems: {}
+      items: {}
     }
   },
   created() {
     this.setDataUsingAPI()
   },
   methods: {
+    isInternalLink(path: string): boolean {
+      return !/^https?:\/\//.test(path)
+    },
     async setDataUsingAPI() {
       await this.$axios.get(process.env.newsUrl).then(response => {
-        const json = response.data
-        this.newsItems = json.newsItems
+        this.items = response.data.newsItems
       })
     }
   }
-}
+})
 </script>
 
 <style lang="scss">
@@ -60,6 +85,49 @@ export default {
       @include card-h2();
       &-icon {
         margin: 3px;
+      }
+    }
+  }
+
+  .WhatsNew-list {
+    padding-left: 0;
+    list-style-type: none;
+
+    &-item {
+      &-anchor {
+        text-decoration: none;
+        margin: 5px;
+        font-size: 14px;
+
+        @include lessThan($medium) {
+          display: flex;
+          flex-wrap: wrap;
+        }
+
+        &-time {
+          flex: 0 0 90px;
+
+          @include lessThan($medium) {
+            flex: 0 0 100%;
+          }
+
+          color: $gray-1;
+        }
+
+        &-link {
+          flex: 0 1 auto;
+
+          @include text-link();
+
+          @include lessThan($medium) {
+            padding-left: 8px;
+          }
+        }
+
+        &-ExternalLinkIcon {
+          margin-left: 2px;
+          color: $gray-3 !important;
+        }
       }
     }
   }
